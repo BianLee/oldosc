2574acb2530a41b2cdab2d2555f53a5dbd5f6eb8
Bian <bianlee2004@gmail.com>
2021-04-03 13:03:00 -0700

    google analytics fix

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,7 @@ import App from "./App";
 // import './index.css'
 import "react-datepicker/dist/react-datepicker.css";
 import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-MKD2E181BM");
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
